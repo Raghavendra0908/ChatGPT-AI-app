3d3f2884ac224be8575487fda35bd40ed1c2fc73
online shop <you@example.com>
2023-06-17 13:45:28 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://chatgpt-ai-j6z3.onrender.com",
+            "https://chatgpt-app-3rpy.onrender.com",
             // "http://localhost:5173",
             { input },
             {
